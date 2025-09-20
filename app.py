a897fb1d77cd6a0f4aff033aabf8b388a6266b22
Muhammad Yousaf <122126603+myousaf179@users.noreply.github.com>
2025-09-20 18:21:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import os
 
 # Load trained pipeline (trainable_file.joblib)
-model_path = os.path.join(os.path.dirname(file), "trainable_file.joblib")
+model_path = os.path.join(os.path.dirname(__file__), "trainable_file.joblib")
 pipeline = joblib.load(model_path)
 
 st.set_page_config(page_title="IMDB Sentiment Tester", layout="centered")
@@ -28,4 +28,5 @@ if submitted:
             st.markdown(f"*Prediction:* :{'green' if sentiment=='Positive' else 'red'}[{sentiment}]")
         except Exception as e:
             st.error(f"Error: {e}")
+
 
